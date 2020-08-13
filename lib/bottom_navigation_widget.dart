348a252de20ac9152bc8bd572b350df4f58b2f8e
custer-go <custertian@163.com>
2020-08-13 21:59:41 +0800

    底部导航栏的页面跳转

--- lib/bottom_navigation_widget.dart
+++ lib/bottom_navigation_widget.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'pages/home_screen.dart';
+import 'pages/airplay_screen.dart';
+import 'pages/pages_screen.dart';
+import 'pages/email_screen.dart';
 
 class BottomNavigationWidget extends StatefulWidget {
   @override
@@ -7,40 +11,65 @@ class BottomNavigationWidget extends StatefulWidget {
 
 class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
   final _bottomNavigationColor = Colors.lightBlue;
+  int _curentIndex = 0; // 索引
+  List<Widget> list = List();
+
+  @override
+  void initState() {
+    list
+      ..add(HomeScreen())
+      ..add(EmailScreen())
+      ..add(PagesScreen())
+      ..add(AirplayScreen());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.home,
-            color: _bottomNavigationColor,
+      body: list[_curentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _bottomNavigationColor,
+            ),
+            title:
+                Text('Home', style: TextStyle(color: _bottomNavigationColor)),
           ),
-          title: Text('Home', style: TextStyle(color: _bottomNavigationColor)),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.email,
-            color: _bottomNavigationColor,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.email,
+              color: _bottomNavigationColor,
+            ),
+            title:
+                Text('Email', style: TextStyle(color: _bottomNavigationColor)),
           ),
-          title: Text('Email', style: TextStyle(color: _bottomNavigationColor)),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.pages,
-            color: _bottomNavigationColor,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.pages,
+              color: _bottomNavigationColor,
+            ),
+            title:
+                Text('Pages', style: TextStyle(color: _bottomNavigationColor)),
           ),
-          title: Text('Pages', style: TextStyle(color: _bottomNavigationColor)),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.airplay,
-            color: _bottomNavigationColor,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.airplay,
+              color: _bottomNavigationColor,
+            ),
+            title: Text('Airplay',
+                style: TextStyle(color: _bottomNavigationColor)),
           ),
-          title:
-              Text('Airplay', style: TextStyle(color: _bottomNavigationColor)),
-        ),
-      ], type: BottomNavigationBarType.fixed),
+        ],
+        currentIndex: _curentIndex,
+        onTap: (int index) {
+          setState(() {
+            _curentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
