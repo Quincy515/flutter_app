2d8900a287114107e5e3c1bae3051cbb26a62d2c
custer-go <custertian@163.com>
2020-08-13 15:30:32 +0800

    动态列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(
+    // items: List<String>(1000), // 固定长度和非固定长度的数组,固定类型，直接传内容【1，2，3】
+    items: new List<String>.generate(1000, (index) => "Item $index")));
 
 class MyApp extends StatelessWidget {
+  final List<String> items;
+  MyApp({Key key, @required this.items}) : super(key: key); // super 调用父类
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,17 +14,11 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         // 脚手架组件
         appBar: new AppBar(title: new Text("ListView Widget")),
-        body: new ListView(
-          children: <Widget>[
-            new ListTile(
-                leading: new Icon(Icons.perm_camera_mic),
-                title: new Text('perm_camera_mic')),
-            new ListTile(
-                leading: new Icon(Icons.add_call), title: new Text('add_call')),
-            new ListTile(
-                leading: new Icon(Icons.access_time),
-                title: new Text('access_time'))
-          ],
+        body: new ListView.builder(
+          itemCount: items.length, // 传递过来参数的长度
+          itemBuilder: (context, index) {
+            return new ListTile(title: new Text("${items[index]}"));
+          },
         ),
       ),
     );
