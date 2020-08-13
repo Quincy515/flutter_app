e7e6713ca36b7c14b3061039a4cb3112e3f85869
custer-go <custertian@163.com>
2020-08-13 14:57:42 +0800

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,28 @@
 import 'package:flutter/material.dart';
 
-// 主方法，入口
 void main() => runApp(MyApp());
 
-// MyApp 继承无状态组件 StatelessWidget
 class MyApp extends StatelessWidget {
-  @override // 重写
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        // 返回 MaterialApp 组件
-        title: 'Text widget',
-        home: Scaffold(
-            appBar: AppBar(title: Text('Welcome to Fultter 欢迎')),
-            body: Center(
-              child: Container(
-                child: new Image.network(
-                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-                  // fit: BoxFit.cover,
-                  color: Colors.greenAccent,
-                  colorBlendMode: BlendMode.darken, // 图片混合模式
-                  repeat: ImageRepeat.repeat,
-                ),
-                width: 300.0,
-                height: 200.0,
-                color: Colors.lightBlue,
-              ),
-            )));
+      title: "Flutter Demo",
+      home: Scaffold(
+        // 脚手架组件
+        appBar: new AppBar(title: new Text("ListView Widget")),
+        body: new ListView(
+          children: <Widget>[
+            new ListTile(
+                leading: new Icon(Icons.perm_camera_mic),
+                title: new Text('perm_camera_mic')),
+            new ListTile(
+                leading: new Icon(Icons.add_call), title: new Text('add_call')),
+            new ListTile(
+                leading: new Icon(Icons.access_time),
+                title: new Text('access_time'))
+          ],
+        ),
+      ),
+    );
   }
 }
-
-// 使用命令行在项目更目录下运行 flutter run
-// p 键显示网格
-// o 键切换 ios、android、fuchsia
-// r 键 重新加载或 vscode 运行 debug 模式
