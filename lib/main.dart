44431097e0f81eb8132a44ad508bb498fce1db3d
custer-go <custertian@163.com>
2020-08-13 18:52:50 +0800

    卡片组件布局

--- lib/main.dart
+++ lib/main.dart
@@ -5,32 +5,50 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var stack = new Stack(
-      alignment: const FractionalOffset(0.5, 0.9),
-      children: <Widget>[
-        new CircleAvatar(
-          backgroundImage: new NetworkImage(
-              'https://avatars0.githubusercontent.com/u/7986197?s=460&u=e84e0aa9a4ff4708964ecf365626e6f5a5895b4f&v=4'),
-          radius: 100.0,
-        ),
-        new Positioned(
-          top: 10.0,
-          left: 60.0,
-          child: new Text('Custer 学习 '),
-        ),
-        new Positioned(
-          bottom: 10.0,
-          right: 10.0,
-          child: new Text('flutter'),
-        ),
-      ],
+    var card = new Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(
+                "Flutter 是 Google 开源的 UI 工具包，帮助开发者通过一套代码库高效构建多平台精美应用，支持移动、Web、桌面和嵌入式平台。",
+                style: TextStyle(fontWeight: FontWeight.w500)),
+            subtitle: Text('好好学习，争取早日做出自己的产品'),
+            leading: new Icon(
+              Icons.flight,
+              color: Colors.lightBlue,
+            ),
+          ),
+          new Divider(),
+          ListTile(
+            title: Text(
+                "Flutter 是 Google 开源的 UI 工具包，帮助开发者通过一套代码库高效构建多平台精美应用，支持移动、Web、桌面和嵌入式平台。",
+                style: TextStyle(fontWeight: FontWeight.w500)),
+            subtitle: Text('好好学习，争取早日做出自己的产品'),
+            leading: new Icon(
+              Icons.flight,
+              color: Colors.lightBlue,
+            ),
+          ),
+          new Divider(),
+          ListTile(
+            title: Text(
+                "Flutter 是 Google 开源的 UI 工具包，帮助开发者通过一套代码库高效构建多平台精美应用，支持移动、Web、桌面和嵌入式平台。",
+                style: TextStyle(fontWeight: FontWeight.w500)),
+            subtitle: Text('好好学习，争取早日做出自己的产品'),
+            leading: new Icon(
+              Icons.flight,
+              color: Colors.lightBlue,
+            ),
+          ),
+        ],
+      ),
     );
     return MaterialApp(
       title: 'Row Widget Demo',
       home: Scaffold(
         appBar: new AppBar(title: new Text("垂直方向布局")),
         body: Center(
-          child: stack,
+          child: card,
         ),
       ),
     );
