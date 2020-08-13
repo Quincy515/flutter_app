a040c786f00f2729fc68d0a6b97f6dadf35e3816
custer-go <custertian@163.com>
2020-08-13 19:41:55 +0800

    页面跳转并返回数据

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,63 @@
 import 'package:flutter/material.dart';
 
-class Product {
-  // 商品抽象
-  final String title; // 商品标题
-  final String description; // 商品描述
-  Product(this.title, this.description); // 构造函数
-}
-
 void main() {
-  runApp(MaterialApp(
-      title: '导航的数据传递和接收',
-      home: ProductList(
-          products: List.generate(
-              20, (index) => Product('商品 $index', '这是一个商品详情，编号为: $index')))));
+  runApp(MaterialApp(title: '页面跳转返回数据', home: FirstPage()));
 }
 
-class ProductList extends StatelessWidget {
-  final List<Product> products; // 参数接收
-  ProductList({Key key, @required this.products}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('商品列表')),
-        body: ListView.builder(
-          itemCount: products.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(products[index].title),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            ProductDetail(product: products[index])));
-              }, // 点击详情事件
-            );
-          },
-        ));
-  }
-}
-
-class ProductDetail extends StatelessWidget {
-  final Product product;
-  ProductDetail({Key key, @required this.product}) : super(key: key);
-
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('${product.title}'),
+          title: Text('客户电话'),
         ),
-        body: Center(child: Text('${product.description}')));
+        body: Center(
+          child: RouteButton(),
+        ));
+  }
+}
+
+class RouteButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+        onPressed: () {
+          _navigateToCustom(context);
+        },
+        child: Text('去找客户'));
+  }
+
+  _navigateToCustom(BuildContext context) async {
+    final result = await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => Custom()));
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text('$result'),
+    ));
+  }
+}
+
+class Custom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('我是客户'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context, '上海客户: 1511008888');
+                  },
+                  child: Text('上海客户')),
+              RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context, '北京客户: 1511008888');
+                  },
+                  child: Text('北京客户'))
+            ],
+          ),
+        ));
   }
 }
