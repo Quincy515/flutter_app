1e5aed933d7c20f6ce6f26ef944df2b7ebcbfc09
custer-go <custertian@163.com>
2020-08-20 22:50:20 +0800

    lib_09.展开闭合ExpansionTile

---
+++ demo/lib_09.展开闭合ExpansionTile/expansion_tile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ExpansionTileDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('expansion title demo'),
+      ),
+      body: Center(
+        child: ExpansionTile(
+          title: Text('Expansion Tile'),
+          leading: Icon(Icons.ac_unit),
+          backgroundColor: Colors.white12,
+          children: <Widget>[
+            ListTile(
+              title: Text('list tile'),
+              subtitle: Text('subtitle'),
+            ),
+          ],
+          initiallyExpanded: true,
+        ),
+      ),
+    );
+  }
+}
