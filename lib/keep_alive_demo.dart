fcc64a9359e9ab4d1be647f3f880df6d928b44ee
custer-go <custertian@163.com>
2020-08-20 21:12:47 +0800

    保持页面状态

---
+++ lib/keep_alive_demo.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+//混入AutomaticKeepAliveClientMixin，这是保持状态的关键
+//然后重写wantKeppAlive 的值为true。
+class _MyHomePageState extends State<MyHomePage>
+    with AutomaticKeepAliveClientMixin {
+  int _counter = 0;
+  //重写 keepAlive 方法为ture ，就是可以有记忆功能了。
+  @override
+  bool get wantKeepAlive => true;
+  //声明一个内部方法，用来点击按钮后增加数量
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // 居中主轴对齐
+          children: <Widget>[
+            Text('点一下加1，点一下加1：'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.bodyText1,
+            )
+          ],
+        ),
+      ),
+      // 增加一个悬浮按钮，点击时触犯_incrementCounter方法
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
