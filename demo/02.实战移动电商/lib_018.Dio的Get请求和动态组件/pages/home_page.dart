a2e1ad104ec2cd1470f616ca1490f87517c42fde
custer-go <custertian@163.com>
2020-08-24 10:42:33 +0800

    SingleChildScrollView解决纵向超出了边界

--- demo/02.实战移动电商/lib_018.Dio的Get请求和动态组件/pages/home_page.dart
+++ demo/02.实战移动电商/lib_018.Dio的Get请求和动态组件/pages/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,33 +13,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
+        child: Scaffold(
           appBar: AppBar(
             title: Text('学海无涯'),
           ),
-          body: Container(
-            child: Column(children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '技术类型',
-                  helperText: '请输入你喜欢的技术',
-                ),
-                autofocus: false, // 自动对焦关闭，避免自动打开手机输入
-              ),
-              RaisedButton(
-                onPressed: _choiceAction,
-                child: Text('选择完毕'),
-              ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis, // 省略号
-                maxLines: 1, // 1行
-              )
-            ]),
-          )),
-    );
+          body: SingleChildScrollView( // 滚动组件避免像素超出容器
+              child: Container(
+                child: Column(children: <Widget>[
+                  TextField(
+                    controller: typeController,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10.0),
+                      labelText: '技术类型',
+                      helperText: '请输入你喜欢的技术',
+                    ),
+                    autofocus: false, // 自动对焦关闭，避免自动打开手机输入
+                  ),
+                  RaisedButton(
+                    onPressed: _choiceAction,
+                    child: Text('选择完毕'),
+                  ),
+                  Text(
+                    showText,
+                    overflow: TextOverflow.ellipsis, // 省略号
+                    maxLines: 1, // 1行
+                  )
+                ]),
+              )),
+        ));
   }
 
   void _choiceAction() {
@@ -48,13 +49,13 @@ class _HomePageState extends State<HomePage> {
       showDialog(
           context: context,
           builder: (context) => AlertDialog(
-                title: Text('类型不能为空'),
-              ));
+            title: Text('类型不能为空'),
+          ));
     } else {
       getHttp(typeController.text.toString()).then((value) => setState(() {
 //            showText = value['data']['name'];
-            showText = value.toString();
-          }));
+        showText = value.toString();
+      }));
     }
   }
 
@@ -62,7 +63,8 @@ class _HomePageState extends State<HomePage> {
     try {
       Response response;
       var data = {'name': TypeText};
-      response = await Dio().get('http://192.168.3.24:2020/ping', queryParameters: data);
+      response = await Dio()
+          .get('http://192.168.3.24:2020/ping', queryParameters: data);
       print(response);
       return response;
     } catch (e) {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,33 +13,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text('学海无涯'),
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text('学海无涯'),
+      ),
+      body: SingleChildScrollView( // 滚动组件避免像素超出容器
+          child: Container(
+        child: Column(children: <Widget>[
+          TextField(
+            controller: typeController,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10.0),
+              labelText: '技术类型',
+              helperText: '请输入你喜欢的技术',
+            ),
+            autofocus: false, // 自动对焦关闭，避免自动打开手机输入
           ),
-          body: Container(
-            child: Column(children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '技术类型',
-                  helperText: '请输入你喜欢的技术',
-                ),
-                autofocus: false, // 自动对焦关闭，避免自动打开手机输入
-              ),
-              RaisedButton(
-                onPressed: _choiceAction,
-                child: Text('选择完毕'),
-              ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis, // 省略号
-                maxLines: 1, // 1行
-              )
-            ]),
-          )),
-    );
+          RaisedButton(
+            onPressed: _choiceAction,
+            child: Text('选择完毕'),
+          ),
+          Text(
+            showText,
+            overflow: TextOverflow.ellipsis, // 省略号
+            maxLines: 1, // 1行
+          )
+        ]),
+      )),
+    ));
   }
 
   void _choiceAction() {
@@ -62,7 +63,8 @@ class _HomePageState extends State<HomePage> {
     try {
       Response response;
       var data = {'name': TypeText};
-      response = await Dio().get('http://192.168.3.24:2020/ping', queryParameters: data);
+      response = await Dio()
+          .get('http://192.168.3.24:2020/ping', queryParameters: data);
       print(response);
       return response;
     } catch (e) {
