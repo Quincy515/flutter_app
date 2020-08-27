9e008e22b5119aee136606cb8d81302037e75c0f
custer-go <custertian@163.com>
2020-08-27 23:07:28 +0800

    购物车-大体结构布局使用Future组件使用Provider取出本地持久化数据

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,75 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_app/provider/cart.dart';
+import 'package:provider/provider.dart';
 
-class CartPage extends StatefulWidget {
-  @override
-  _CartPageState createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-  List<String> testList = []; // 默认为空显示在页面上的变量
-
+class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    _show(); // 每次进入前进行显示
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 500.0,
-            child: ListView.builder(
-              itemCount: testList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(testList[index]),
-                );
-              },
-            ),
-          ),
-          RaisedButton(
-            onPressed: () {
-              _add();
-            },
-            child: Text('增加'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              _clear();
-            },
-            child: Text('清空'),
-          )
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('购物车'),
+      ),
+      body: FutureBuilder(
+        future: _getCartInfo(context),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List cartList = Provider.of<CartProvider>(context).cartList;
+            return ListView.builder(
+                itemCount: cartList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(cartList[index].goodsName),
+                  );
+                });
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
       ),
     );
   }
 
-  // 增加方法
-  void _add() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String temp = 'flutter技术学习';
-    testList.add(temp);
-    // 存入持久化
-    await prefs.setStringList('testInfo', testList);
-    _show();
-  }
-
-  // 查询
-  void _show() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (prefs.getStringList('testInfo') != null) {
-      setState(() {
-        testList = prefs.getStringList('testInfo');
-      });
-    }
-  }
-
-  // 删除
-  void _clear() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    // prefs.clear(); // 所有的都删除了
-    prefs.remove('testInfo'); // 只移除 key
-    setState(() {
-      testList = [];
-    });
+  Future<String> _getCartInfo(BuildContext context) async {
+    await Provider.of<CartProvider>(context).getCartInfo();
+    return 'end';
   }
 }
