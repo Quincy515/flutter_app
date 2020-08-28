9bede43395d2cc64437b3360d9f6082e67303d8e
custer-go <custertian@163.com>
2020-08-28 16:27:03 +0800

    lib_043.会员中心UI布局

---
+++ demo/02.实战移动电商/lib_043.会员中心UI布局/routers/router_handler.dart
@@ -0,0 +1,13 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+
+import '../pages/details_page.dart';
+
+// Handler 控制器或操作器
+Handler detailsHandler = Handler(
+  handlerFunc: (BuildContext context, Map<String, List<String>> param){
+    String goodsId = param['id'].first;
+    print('index>details goodsId is ${goodsId}');
+    return DetailsPage(goodsId);
+  }
+);
