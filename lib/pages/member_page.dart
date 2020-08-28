8541b92614113dddf83c6fb08305953773b17203
custer-go <custertian@163.com>
2020-08-28 16:19:29 +0800

    会员中心—订单区域UI编写

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -11,6 +11,8 @@ class MemberPage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           _topHeader(),
+          _orderTitle(),
+          _orderType(),
         ],
       ),
     );
@@ -39,6 +41,80 @@ class MemberPage extends StatelessWidget {
                   fontSize: ScreenUtil().setSp(36), color: Colors.black54),
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  // 我的订单
+  Widget _orderTitle() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text('我的订单'),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+  // 订单类型
+  Widget _orderType() {
+    return Container(
+      margin: EdgeInsets.only(top: 5),
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.only(top: 20),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.party_mode, size: 30),
+                Text('待付款'),
+              ],
+            ),
+          ),
+
+          ///--------------------------------
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.query_builder, size: 30),
+                Text('待发货'),
+              ],
+            ),
+          ),
+
+          ///--------------------------------
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.directions_car, size: 30),
+                Text('待收货'),
+              ],
+            ),
+          ),
+
+          ///--------------------------------
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.content_paste, size: 30),
+                Text('待评价'),
+              ],
+            ),
+          ),
+
+          ///--------------------------------
         ],
       ),
     );
