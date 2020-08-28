6b7069e25a5c54f3e5ceb6c8e0e3c179e7b54c87
custer-go <custertian@163.com>
2020-08-28 16:07:39 +0800

    会员中心-首页头部布局

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../provider/counter.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text(
-        /// Calls `context.watch` to make [MyHomePage] rebuild when [Counter] changes.
-        '${context.watch<Counter>().value}',
-        style: Theme.of(context).textTheme.headline1,
+      appBar: AppBar(
+        title: Text('会员中心'),
       ),
-    ));
+      body: ListView(
+        children: <Widget>[
+          _topHeader(),
+        ],
+      ),
+    );
+  }
+
+  // 头像区域
+  Widget _topHeader() {
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: Colors.pinkAccent,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: ClipOval(
+              child: Image.network(
+                  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598611846969&di=b4863ad9563c9b6252c8afb3b9f948dd&imgtype=0&src=http%3A%2F%2Fb.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fbd315c6034a85edf8c1758e240540923dc547553.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              'Flutter学习',
+              style: TextStyle(
+                  fontSize: ScreenUtil().setSp(36), color: Colors.black54),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
