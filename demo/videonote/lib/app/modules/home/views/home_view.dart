05327e438627f8b21eedf942327f35e01ac7dc9d
Custer <custertian@163.com>
2022-08-19 13:31:36 +0800

    update

--- demo/videonote/lib/app/modules/home/views/home_view.dart
+++ demo/videonote/lib/app/modules/home/views/home_view.dart
@@ -80,11 +80,11 @@ class HomeView extends GetView<HomeController> {
                 ),
                 Row(
                   children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      height: MediaQuery.of(context).size.height * 0.6,
-                      child: VideoPlayer(),
-                    ),
+                    // SizedBox(
+                    //   width: MediaQuery.of(context).size.width * 0.7,
+                    //   height: MediaQuery.of(context).size.height * 0.6,
+                    //   child: VideoPlayer(),
+                    // ),
                     Expanded(
                         child: SizedBox(
                       height: MediaQuery.of(context).size.height * 0.6,
